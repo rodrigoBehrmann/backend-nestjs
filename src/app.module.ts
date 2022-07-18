8fd3b312516a412d0cb58b08611c10f0279e9c24
Rodrigo Behrmann <54535102+rodrigoBehrmann@users.noreply.github.com>
2022-07-18 17:17:53 -0300

    Servidor MongoDB configurado
    Rotas adicionadas e funcionando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://rodrigo:SosqS1lI35v1ZIT4@cluster0.j5p1b.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
